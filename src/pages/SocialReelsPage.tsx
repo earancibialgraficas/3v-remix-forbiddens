da316ce6f2b454b7a268ebc1c7306502900af04c
earancibialgraficas <e.arancibial.graficas@gmail.com>
2026-04-29 16:47:53 -0400

    padding

--- src/pages/SocialReelsPage.tsx
+++ src/pages/SocialReelsPage.tsx
@@ -424,7 +424,7 @@ function SnapCard({
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              padding: '5vh 0 15vh 0',
+              padding: '5vh 0 8vh 0',
               height: '100vh',
               boxSizing: 'border-box',
               width: '100%'
